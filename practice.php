1e2ca267d98405c4862a10434e4d4d05bca41035
chihiro hidaka <chihi.stu270@gmail.com>
2023-02-03 14:04:29 +0000

    課題

---
+++ practice.php
@@ -0,0 +1,12 @@
+<?php
+echo 'hello php!';
+$a = 3;
+$b = 7;
+echo $a+$b;
+
+
+
+
+
+
+
---
+++ practice0203.php
@@ -0,0 +1,25 @@
+<?php
+$weekday="月曜";
+switch($weekday){
+    case"月曜”：
+     echo  "可燃ごみの日です。";
+     break;
+    case "水曜"：
+     echo"資源ごみの日です";
+     break
+    default:
+     echo"回収はありません"；
+    break;
+}
+
+
+$total=0;
+echo $total;
+
+for($1=0;$1<=100;$1++){
+    $total += $i;
+}
+
+echo $total;
+
+}
